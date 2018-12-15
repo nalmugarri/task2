1afc78361c15ee738cbd6867c65d9d45392ddd9a
Noura Khaled <nalmugarri@nouras-macbook-pro.local>
2018-12-15 17:44:25 +0300

    first commit

---
+++ task2/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  task2
+//
+//  Created by Noura Khaled on 11/12/2018.
+//  Copyright © 2018 Noura Almugarri. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ task2/FlickrPhotosViewController.swift
@@ -0,0 +1,223 @@
+//
+//  FlickrPhotosViewController.swift
+//  task2
+//
+//  Created by Noura Khaled on 11/12/2018.
+//  Copyright © 2018 Noura Almugarri. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UICollectionViewController {
+    
+    fileprivate let reuseIdentifier = "FlickrCell"
+    
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    fileprivate var searches = [FlickrSearchResults]()
+    // is an array that will keep track of all the searches made in the app
+    
+    fileprivate let flickr = Flickr()
+    // is a reference to the object that will do the searching for me
+    
+    fileprivate let itemsPerRow: CGFloat = 3
+    
+    // MARK: - Private
+
+}
+
+
+//private extension FlickrPhotosViewController {
+//    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+//        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+//
+//        /* is a convenience method that will get a specific photo related to an index path in the collection view. i'm going to access a photo for a specific index path a lot, so that help to not repeat the code.
+//        */
+//    }
+//}
+
+
+extension FlickrPhotosViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(style: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            
+            activityIndicator.removeFromSuperview()
+            
+            
+            if let error = error {
+                // 2
+                print("Error searching : \(error)")
+                return
+            }
+            
+            if let results = results {
+                // 3
+                print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+                self.searches.insert(results, at: 0)
+                
+                // 4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension FlickrPhotosViewController {
+    //1
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//        cell.backgroundColor = UIColor.black
+//        // Configure the cell
+//        return cell
+//    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //1
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FlickrPhotoCell
+        //2
+        let flickrPhoto = photoForIndexPath(indexPath: indexPath)
+        cell.backgroundColor = UIColor.white
+        //3
+        cell.imageView.image = flickrPhoto.thumbnail
+        
+        return cell
+    }
+}
+
+extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
+
+// MARK: - Private
+private extension FlickrPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
+}
+
+
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Register cell classes
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using [segue destinationViewController].
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//    // MARK: UICollectionViewDataSource
+//
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of items
+//        return 0
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//
+//        // Configure the cell
+//
+//        return cell
+//    }
+//
+//    // MARK: UICollectionViewDelegate
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be highlighted during tracking
+//    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be selected
+//    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+//    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+//
+//    }
+//    */
+//
+//}
